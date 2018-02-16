d4258cb6c39f7fbccf2d4dec6314362cb6d1ff92
WC\jlongden <longden.jonathanedgar@gmail.com>
2018-02-15 23:05:02 -0700

    extention working but without A-Frame

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import 'aframe';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Example from "./Aframe";
+
 
 class App extends Component {
   render() {
@@ -11,13 +12,41 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          <Example />
-        </p>
-      </div>
+        <a-scene>
+        <a-entity 
+            geometry="primitive: box" 
+            position="-1 0.5 -3" 
+            rotation="0 45 0" 
+            material="color: #4CC3D9" />
+          <a-entity 
+            geometry="primitive: sphere; radius: 1.25;" 
+            position="0 1.25 -5" 
+            material="color: #EF2D5E" />
+          <a-entity 
+            geometry="primitive: cylinder; radius: 0.5, height: 1.5" 
+            position="1 0.75 -3" 
+            material="color: #FFC65D" />
+          <a-entity 
+            geometry="primitive: plane; width: 4; height: 4" 
+            position="0 0 -4" 
+            rotation="-90 0 0" 
+            material="color: #7BC8A4" />
+        </a-scene>
+        </div>
     );
   }
 }
 
 export default App;
-//To get started, edit <code>src/App.js</code> and save to reload.
+
+
+
+// <div className="App">
+//   <header className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//     <h1 className="App-title">Welcome to React</h1>
+//   </header>
+//   <p className="App-intro">
+//     To get started, edit <code>src/App.js</code> and save to reload.   
+//   </p>
+// </div>

bec0a29757c2605fef0a122a502886b91457010f
WC\jlongden <longden.jonathanedgar@gmail.com>
2018-02-15 22:23:08 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import AframeExample from './aframe';
+import Example from "./Aframe";
 
 class App extends Component {
   render() {
@@ -12,12 +12,12 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Example />
         </p>
-        <AframeExample />
       </div>
     );
   }
 }
 
 export default App;
+//To get started, edit <code>src/App.js</code> and save to reload.
--- src/aframe.js
+++ src/aframe.js
@@ -1,19 +1,23 @@
 import 'aframe';
-import 'aframe-particle-system-component';
-import {Entity, Scene} from 'aframe-react';
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import {Box, Sphere, Cylinder, Plane, Sky, Text, Scene} from 'react-aframe-ar';
  
-class VRScene extends React.Component {
+ 
+class Example extends Component {
   render () {
-    return (
-        <Scene>
-        <Entity
-          geometry={{primitive: 'box', width: 5}}
-          material={{color: red, roughness: 0.5, src: texture.png}}
-          scale={{x: 2, y: 2, z: 2}}
-          position={{x: 0, y: 0, z: -5}}/>
+    return (<div>
+      <Scene>
+        <Box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" shadow />
+        <Sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E" shadow />
+        <Cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D" shadow />
+        <Plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4" shadow />
+        <Sky color="#ECECEC" />
+        <Text value="Hello world, react-aframe-ar!" align="center" position="0 2.3 -1.5" color="#7BC8A4" />
       </Scene>
+      </div>
     );
   }
 }
+
+export default Example;
